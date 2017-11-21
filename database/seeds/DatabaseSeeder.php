9b441b54ba819e6df37a81221fb67656d66de2c5
Shuvosd <shuvosutradhar00@gmail.com>
2017-11-21 10:15:29 +0600

    codehacking updated laravel 5.4

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+
+    	// factory(App\User::class, 10)->create()->each(function($user){
+    	// 	$user->posts()->save(factory(App\Post::class)->make());
+    	// })
+    	factory(App\User::class, 10)->create();
+        // $this->call(UsersTableSeeds::class);
     }
 }
